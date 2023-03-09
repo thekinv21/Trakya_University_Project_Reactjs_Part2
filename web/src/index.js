ba15bfc44d3eb064ce7baaed6ecf7fcbd8247adc
thekinv21 <vadimkiniabaev@gmail.com>
2023-03-09 03:44:02 +0300

    Added chakra uı Provider

--- web/src/index.js
+++ web/src/index.js
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Routes from "./routes/Routes";
+import { ChakraProvider } from "@chakra-ui/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Routes />
+    <ChakraProvider>
+      <Routes />
+    </ChakraProvider>
   </React.StrictMode>
 );
