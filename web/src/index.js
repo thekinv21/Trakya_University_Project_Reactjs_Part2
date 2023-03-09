5836b7f2cef20602d228c98ff5c241d970154446
thekinv21 <vadimkiniabaev@gmail.com>
2023-03-09 16:30:05 +0300

    add style css

--- web/src/index.js
+++ web/src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Routes from "./routes/Routes";
 import { ChakraProvider } from "@chakra-ui/react";
+import './style/style.css'
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
