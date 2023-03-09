12e1bd6faf77235d2330f7b7c7891129a03d5fae
thekinv21 <vadimkiniabaev@gmail.com>
2023-03-09 22:16:34 +0300

    add signin and signup routes

--- web/src/routes/Routes.jsx
+++ web/src/routes/Routes.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import SignIn from "./../pages/sign-in/SignIn";
+import SignUp from './../pages/sign-up/SignUp'
 
-const Routes = () => {
+const Routers = () => {
   return (
-    <div>Routes</div>
-  )
-}
+    <React.Fragment>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<SignIn />} />
+          <Route path="signin" element={<SignIn/>}/>
+          <Route path='signup' element={<SignUp/>}/>
+        </Routes>
+      </BrowserRouter>
+    </React.Fragment>
+  );
+};
 
-export default Routes
+export default Routers;
