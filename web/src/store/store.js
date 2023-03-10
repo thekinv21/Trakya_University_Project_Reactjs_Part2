0f206f525352a6d0d9fd51937cbd49fcbf0eb036
thekinv21 <vadimkiniabaev@gmail.com>
2023-03-10 23:16:47 +0300

    Restoranlar globale state oluşturuldu

--- web/src/store/store.js
+++ web/src/store/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import LinksSlicer from "../redux/LinksSlicer";
+import RestaurantSlicer from "../redux/RestaurantSlicer";
 
 export const store = configureStore({
   reducer: {
     Link: LinksSlicer,
+    Restaurant: RestaurantSlicer,
   },
 });
