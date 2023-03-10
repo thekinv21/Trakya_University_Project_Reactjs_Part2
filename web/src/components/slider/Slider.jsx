74dc36a8432d553422e31eda3867448c67cec039
thekinv21 <vadimkiniabaev@gmail.com>
2023-03-10 23:18:31 +0300

    Slider componenti oluşturuldu

---
+++ web/src/components/slider/Slider.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+const SliderSlick = ({ children }) => {
+
+  //*--------------------Slider Ayarları için----------------------*/}
+
+  const sliderSettings = {
+    infinite: true,
+    slidesToShow: 6,
+    speed: 500,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 6,
+          slidesToScroll: 6,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 320,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+  return (
+    <React.Fragment>
+      <Slider {...sliderSettings}>{children}</Slider>
+    </React.Fragment>
+  );
+};
+
+export default SliderSlick;
