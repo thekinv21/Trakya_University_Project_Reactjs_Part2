0b821003ff754e0846ab65d1ec5f4353e5042e40
thekinv21 <vadimkiniabaev@gmail.com>
2023-03-10 23:21:06 +0300

    Restoranlar , MenuCategory vb Main Page Eklendi

--- web/src/pages/main/Main.jsx
+++ web/src/pages/main/Main.jsx
@@ -2,16 +2,25 @@ import React from "react";
 import MainContainer from "./../../components/containers/main-container/Main-containers";
 import Header from "./components/header/Header";
 import Banner from "./components/banner/Banner";
-import About from './../about/About';
+import About from "./../about/About";
+import Restaurants from "./components/restaurants/Restaurants";
+import Category from "./components/category/Category";
 
 const Main = () => {
   return (
     <React.Fragment>
+      {/*--------------------Landing Header-------------------- */}
       <Header />
-
+      {/*--------------------Landing Container-------------------- */}
       <MainContainer>
+        {/*--------------------Landing Banner-------------------- */}
         <Banner />
-        <About/>
+        {/*--------------------RestaurantsList-------------------- */}
+        <Restaurants />
+        {/*--------------------AboutUs-------------------- */}
+        <About />
+        {/*--------------------Menus Category-------------------- */}
+        <Category />
       </MainContainer>
     </React.Fragment>
   );
