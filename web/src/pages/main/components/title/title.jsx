12fc1f39735d3e6950de8e8ccf7e3d470a4e429b
thekinv21 <vadimkiniabaev@gmail.com>
2023-03-10 17:01:50 +0300

    create landingPage title container

--- web/src/pages/main/components/title/title.jsx
+++ web/src/pages/main/components/title/title.jsx
@@ -1,25 +1,26 @@
 import React from "react";
-import { Heading, Divider } from "@chakra-ui/react";
+import { Heading, Divider, Stack } from "@chakra-ui/react";
 
 const Title = ({ title }) => {
   return (
     <React.Fragment>
-      <Heading
-        color="rgb(0,0,0,0.6)"
-        fontWeight="600"
-        fontFamily="unset"
-        pb={5}
-      >
-        {title}
-      </Heading>
+      <Stack>
+        <Heading
+          color="rgb(0,0,0,0.6)"
+          fontWeight="600"
+          fontFamily="unset"
+          pb={5}
+        >
+          {title}
+        </Heading>
 
-      <Divider
-        orientation="horizontal"
-        colorScheme="red"
-        w={["300px", "400px", "500px", "700px"]}
-        border="1px solid orange"
-        m="30px 0px"
-      />
+        <Divider
+          orientation="horizontal"
+          colorScheme="red"
+          w={["300px", "400px", "500px", "700px"]}
+          border="1px solid orange"
+        />
+      </Stack>
     </React.Fragment>
   );
 };
