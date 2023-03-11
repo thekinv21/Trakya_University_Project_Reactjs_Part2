bec4fe7ab3e7e85a795eb1fe80ba9cf3e836adc2
thekinv21 <vadimkiniabaev@gmail.com>
2023-03-12 01:45:39 +0300

    404 ve Profile sayfa eklendi

--- web/src/routes/Routes.jsx
+++ web/src/routes/Routes.jsx
@@ -3,16 +3,20 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import SignIn from "./../pages/sign-in/SignIn";
 import SignUp from "./../pages/sign-up/SignUp";
 import Main from "../pages/main/Main";
+import Notfound from "./../pages/notfound/Notfound";
+import Profile from "./../pages/profile/Profile";
 
 const Routers = () => {
   return (
     <React.Fragment>
       <BrowserRouter>
         <Routes>
+          <Route path="*" element={<Notfound />} />
           <Route path="/" element={<SignIn />} />
           <Route path="/signin" element={<SignIn />} />
           <Route path="/signup" element={<SignUp />} />
           <Route path="/main" element={<Main />} />
+          <Route path="/profile" element={<Profile />} />
         </Routes>
       </BrowserRouter>
     </React.Fragment>
