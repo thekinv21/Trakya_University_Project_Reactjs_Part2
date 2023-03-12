1ff303a07147bc5ceecf511a78603b9b03c8f06a
thekinv21 <vadimkiniabaev@gmail.com>
2023-03-12 21:22:42 +0300

    validasyonlar değiştirildi

--- web/src/components/forms/login-forms/validation/sign-in.js
+++ web/src/components/forms/login-forms/validation/sign-in.js
@@ -4,7 +4,7 @@ import * as yup from 'yup'
 const signInSchema = yup.object().shape({
 
     username : yup.string().required('Username boş birakilamaz'),
-    password : yup.string().min(6).required('En az 6 karakter içermeli')
+    password : yup.string().required('En az 6 karakter içermeli')
 
 })
 
