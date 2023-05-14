b824f67274915d9ca5728356384516c0e5168c48
thekinv21 <vadimkiniabaev@gmail.com>
2023-05-15 01:33:17 +0300

    update swiperjs

--- web/src/components/swiper/Swiper.js
+++ web/src/components/swiper/Swiper.js
@@ -1,15 +1,23 @@
 import React from "react";
 import { Swiper } from "swiper/react";
 import { Responsive } from "./Responsive";
+import { Pagination } from "swiper";
 import "swiper/css";
+import "swiper/css/pagination";
 
 const Swipers = ({ children }) => {
   return (
     <React.Fragment>
       <Swiper
+        style={{ height: "100%" }}
         breakpoints={Responsive}
         slidesPerView={4}
         spaceBetween={30}
+        freeMode={true}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Pagination]}
         className="mySwiper"
       >
         {children}
