e15228eb8b273ec618d70723c9fe436a6a92bdd8
thekinv21 <vadimkiniabaev@gmail.com>
2023-05-17 22:48:43 +0300

    update indexjs

--- web/src/index.js
+++ web/src/index.js
@@ -3,16 +3,17 @@ import ReactDOM from "react-dom/client";
 import Routes from "./routes/Routes";
 import { ChakraProvider } from "@chakra-ui/react";
 import { Provider } from "react-redux";
-import "./style/style.css";
 import { store } from "./store/store";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "react-toastify/dist/ReactToastify.css";
-// import "antd/dist/antd.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./style/style.css";
 
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       refetchOnWindowFocus: false,
+      cacheTime: Infinity,
     },
   },
 });
