7fce8573833dd56c81eeae96324d9d38d61a0be0
thekinv21 <vadimkiniabaev@gmail.com>
2023-05-17 22:35:14 +0300

    update favorites

--- web/src/pages/favorites/Favorites.js
+++ web/src/pages/favorites/Favorites.js
@@ -1,52 +1,70 @@
-import React from "react";
 import Container from "../../components/container/Container";
 import { Box } from "@chakra-ui/react";
-import { useSelector } from "react-redux";
-import RestCard from "../main/components/restaurant_list/RestCard";
 import UserCard from "./../../components/cards/user/UserCard";
 import NotfoundFavorite from "./components/NotfoundFavorite";
+import { useGetFavorites } from "../../api/restaurant";
+import RestaurantCard from "../main/components/restaurant_list/RestaurantCard/RestaurantCard";
+import { useState } from "react";
+import { useEffect } from "react";
 
 const Favorites = () => {
   //*==============FAVORİTE RESTAURANTS LİST=============
+  const userId = JSON.parse(sessionStorage.getItem("token"));
+  const { data, isLoading } = useGetFavorites(userId.user.id);
 
-  const favorites = useSelector((state) => state.Favorites.favoriteRestaurant);
+  const [filteredData, setFilteredData] = useState([]);
+
+  useEffect(() => {
+    if (isLoading) return;
+    setFilteredData(data?.content);
+  }, [isLoading, data]);
+
+  const onClick = (e) => {
+    console.log("basildi", e);
+    setFilteredData(filteredData.filter((x) => x.id !== e));
+  };
+
+  if (isLoading) return data;
 
   return (
-    <React.Fragment>
-      <Container>
-        {/*=================FAVORİTES CONTAİNER===============*/}
+    <Container>
+      {/*=================FAVORİTES CONTAİNER===============*/}
+      <Box
+        padding={10}
+        className="container"
+        display="flex"
+        alignItems="start"
+        justifyContent="center"
+        gap={5}
+      >
+        {/*=================FAVORİTES USERCARD===============*/}
+
+        <UserCard />
+
+        {/*================FAVORİ RESTAURANT VARSA============ */}
+
         <Box
-          padding={10}
-          className="container"
           display="flex"
-          alignItems="start"
-          justifyContent="center"
+          justifyContent={["center", "center", "center", "start"]}
+          alignItems="center"
+          flexWrap="wrap"
           gap={5}
         >
-          {/*=================FAVORİTES USERCARD===============*/}
-
-          <UserCard />
-
-          {/*================FAVORİ RESTAURANT VARSA============ */}
-
-          <Box
-            display="flex"
-            justifyContent={["center", "center", "center", "start"]}
-            alignItems="center"
-            flexWrap="wrap"
-            gap={5}
-          >
-            {favorites.length ? (
-              favorites?.map((fav, index) => (
-                <RestCard key={index} restaurant={fav} />
-              ))
-            ) : (
-              <NotfoundFavorite />
-            )}
-          </Box>
+          {filteredData?.length ? (
+            filteredData?.map((fav, index) => (
+              <RestaurantCard
+                key={index}
+                onClick={onClick}
+                restaurant={{ ...fav, isFavorite: true }}
+                favData={data.content}
+              />
+            ))
+          ) : (
+            <NotfoundFavorite />
+          )}
         </Box>
-      </Container>
-    </React.Fragment>
+      </Box>
+    </Container>
   );
 };
 
