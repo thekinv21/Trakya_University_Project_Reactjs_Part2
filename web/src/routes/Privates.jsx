19ba6d6f4991206c4d4f0eabacf5f60cb9fd5c58
thekinv21 <vadimkiniabaev@gmail.com>
2023-05-17 22:42:21 +0300

    delete

--- web/src/routes/Privates.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Outlet, Navigate } from "react-router-dom";
-
-export const PrivateRoute = () => {
-  const token = sessionStorage.getItem("token");
-
-  return token ? <Outlet /> : <Navigate to="/sigin" />;
-};
