5f38298cdbcf9668b2e135e59e41527e59bee035
thekinv21 <vadimkiniabaev@gmail.com>
2023-05-17 22:46:54 +0300

    update category requests

--- web/src/api/category.js
+++ web/src/api/category.js
@@ -1,29 +1,33 @@
 import { END_POINTS } from "./end-points";
-import  {useQuery }from '@tanstack/react-query'
-import axios from 'axios'
-
-
-const Token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ2YWRpbSIsImlzcyI6InJlenp0b3JhbiIsImV4cCI6MTY3NzczODAzN30.79BpLEt2N7xqDbHU6AQVCh2-bnAUmbdfj-FIgmuDoRY';
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 
 //========== GET CATEGORY ============
 const getCategory = async () => {
-    const response = await axios.get(END_POINTS.CATEGORY_CONTROLLER.GET_CATEGORY , { headers: {"Authorization" : 'Bearer '  +  Token}})
-    return response.data.content
-}
+  const response = await axios.get(END_POINTS.CATEGORY_CONTROLLER.GET_CATEGORY);
+  return response.data.content;
+};
 
 export const useGetCategory = () => {
-    const {data, refetch, isLoading} = useQuery(['getCategory'], () => getCategory())
-    return {data, refetch, isLoading}
-}
+  const { data, refetch, isLoading } = useQuery(["getCategory"], () =>
+    getCategory()
+  );
+  return { data, refetch, isLoading };
+};
 
 //========== GET CATEGORY BY ID ==========
 const getCategoryById = async (id) => {
-    const URL = END_POINTS.CATEGORY_CONTROLLER.GET_CATEGORY_BY_ID.replace(':id',id)
-    const response = await axios.get(URL)
-    return response.data.content
-}
+  const URL = END_POINTS.CATEGORY_CONTROLLER.GET_CATEGORY_BY_ID.replace(
+    ":id",
+    id
+  );
+  const response = await axios.get(URL);
+  return response.data.content;
+};
 
 export const useGetCategoryById = (id) => {
-    const {data, refetch, isLoading} = useQuery(['getCategoryById', id], () => getCategoryById(id))
-    return {data, refetch, isLoading}
-}
+  const { data, refetch, isLoading } = useQuery(["getCategoryById", id], () =>
+    getCategoryById(id)
+  );
+  return { data, refetch, isLoading };
+};
