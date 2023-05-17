a97535e5a0590af2b1b7ed1544e9657168fb9df9
thekinv21 <vadimkiniabaev@gmail.com>
2023-05-17 22:31:32 +0300

    filtreleme eklendi

--- web/src/pages/discover/Discover.js
+++ web/src/pages/discover/Discover.js
@@ -1,38 +1,116 @@
-import React from "react";
 import Container from "../../components/container/Container";
-import SeacrhCard from "./components/SeacrhCard";
-import FilterCard from "./components/FilterCard";
-import "bootstrap/dist/css/bootstrap.min.css";
+import FilterCard from "./components/RestaurantFilter/FilterCard";
 import { Box } from "@chakra-ui/react";
-import RestaurantCard from "../../components/cards/restaurant/RestaurantCard";
+import { useGetFilteredRest } from "../../api/restaurant";
+import RestSearch from "./components/RestaurantSearch/RestSearch";
+import { useRef, useState } from "react";
+import { useSearchParams } from "react-router-dom";
 
 const Discover = () => {
+  const cityRef = useRef();
+  const restaurantNameRef = useRef();
+
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const [selectedSortOption, setSelectedSortOption] = useState(
+    searchParams.get("sortField" || "")
+  );
+  const page = searchParams.get("page");
+  
+  //*===================PAGİNATİON===================
+  const [currentPage, setCurrentPage] = useState(page || 0);
+
+  const sortField = searchParams.get("sortField") || "";
+  const restaurantName = searchParams.get("restaurantName") || "";
+  const city = searchParams.get("city") || "";
+  const size = searchParams.get("size") || "4";
+
+  console.log(sortField);
+
+  const [initialValues, setInitialValues] = useState({
+    restaurantName: restaurantName,
+    city: city,
+    sortField: sortField,
+  });
+
+  const { data: rest, isLoading } = useGetFilteredRest(
+    city,
+    restaurantName,
+    page - 1,
+    size,
+    sortField
+  );
+
+  const handleClick = async () => {
+    console.log(
+      cityRef.current.input.value,
+      restaurantNameRef.current.input.value,
+      selectedSortOption
+    );
+
+    window.location.href = `/discover?city=${
+      cityRef.current.input.value
+    }&restaurantName=${restaurantNameRef.current.input.value}&page=${
+      page - 1
+    }&size=${size}&sortField=${selectedSortOption}`;
+  };
+
+  const setPage = (e) => {
+    console.log("eeee", e);
+    setSearchParams({
+      ...searchParams,
+      page: e,
+      city: initialValues.city,
+      restaurantName: initialValues.restaurantName,
+      sortField: initialValues.sortField,
+    });
+    setCurrentPage(e);
+  };
+
+  //*===================All RESTAURANTS===================
+
+  if (isLoading) return rest;
+
   return (
-    <React.Fragment>
-      <Container>
+    <Container>
+      <Box
+        className="container"
+        display={["block", "block", "block", "flex"]}
+        gap={5}
+        overflow="hidden"
+        pt={50}
+      >
+        {/*===============DISCOVER SEARCH AND MAP=============== */}
+
         <Box
           className="container"
-          display={["block", "block", "block", "flex"]}
-          gap={5}
-          overflow="hidden"
-          pt={50}
+          display="flex"
+          flexDirection="column"
+          mb={50}
+          maxW={880}
         >
-          {/*===============DISCOVER SEARCH AND MAP=============== */}
-
-          <Box display="flex" flexDirection="column" mb={50}>
-            <SeacrhCard />
-
-            <RestaurantCard />
-          </Box>
-
-          {/*=====================DISCOVER FİLTER================== */}
-
-          <Box className="container">
-            <FilterCard />
-          </Box>
+          <RestSearch
+            cityRef={cityRef}
+            restaurantNameRef={restaurantNameRef}
+            selectedSortOption={selectedSortOption}
+            setSelectedSortOption={setSelectedSortOption}
+            initialValues={initialValues}
+            handleClick={handleClick}
+            page={page}
+            rest={rest}
+            currentPage={currentPage}
+            setCurrentPage={setPage}
+            setInitialValues={setInitialValues}
+          />
         </Box>
-      </Container>
-    </React.Fragment>
+
+        {/*=====================DISCOVER FİLTER================== */}
+
+        <Box>
+          <FilterCard />
+        </Box>
+      </Box>
+    </Container>
   );
 };
 
