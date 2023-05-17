b45428234c4e572bb1074bbfb98030022cbea659
thekinv21 <vadimkiniabaev@gmail.com>
2023-05-17 22:43:52 +0300

    create http requests for restaurant

--- web/src/api/restaurant.js
+++ web/src/api/restaurant.js
@@ -1,26 +1,144 @@
 import { END_POINTS } from "./end-points";
-import { useQuery } from '@tanstack/react-query'
-import axios from 'axios'
+import { useMutation, useQuery } from "@tanstack/react-query";
+import axios from "axios";
 
-//========== GET RESTAURANT ============
+//*========== GET RESTAURANT ============
 const getRestaurant = async () => {
-    const response = await axios.get(END_POINTS.RESTAURANT_CONTROLLER.GET_RESTAURANT)
-    return response.data.content
-}
+  const myToken = getToken();
+  const headers = {
+    Authorization: `Bearer ${myToken}`,
+  };
+
+  const response = await axios.get(
+    END_POINTS.RESTAURANT_CONTROLLER.GET_RESTAURANT,
+    { headers }
+  );
+  return response.data.content;
+};
 
 export const useGetRestaurant = () => {
-    const { data, refetch, isLoading } = useQuery(['getRestaurant'], () => getRestaurant())
-    return { data, refetch, isLoading }
-}
+  const { data, refetch, isLoading } = useQuery(["getRestaurant"], () =>
+    getRestaurant()
+  );
+  return { data, refetch, isLoading };
+};
 
-//========== GET RESTAURANT BY ID ==========
+//*========== GET RESTAURANT BY ID ==========
 const getRestaurantById = async (id) => {
-    const URL = END_POINTS.RESTAURANT_CONTROLLER.GET_RESTAURANT_BY_ID.replace(':id', id)
-    const response = await axios.get(URL)
-    return response.data.content
-}
+  const URL = END_POINTS.RESTAURANT_CONTROLLER.GET_RESTAURANT_BY_ID.replace(
+    ":id",
+    id
+  );
+  const response = await axios.get(URL);
+  return response.data.content;
+};
 
 export const useGetRestaurantById = (id) => {
-    const { data, refetch, isLoading } = useQuery(['getRestaurant', id], () => getRestaurantById(id))
-    return { data, refetch, isLoading }
-}
+  const { data, refetch, isLoading } = useQuery(["getRestaurant", id], () =>
+    getRestaurantById(id)
+  );
+  return { data, refetch, isLoading };
+};
+
+//*========== GET RESTAURANT EMPTY DATE ==========
+
+const getEmptyHours = async (id, date) => {
+  const URL =
+    END_POINTS.RESTAURANT_CONTROLLER.GET_RESTAURANT_EMPTY_HOURS.replace(
+      ":id",
+      id
+    ).replace(":date", date);
+
+  const response = await axios.get(URL);
+  return response.data.content;
+};
+
+export const useGetEmptyHours = (id, date) => {
+  const { data, isLoading } = useQuery(["getEmptyHours", id, date], () =>
+    getEmptyHours(id, date)
+  );
+  return { data, isLoading };
+};
+
+//*============ GET FİLTERED RESTAURANT==============
+
+const getFilterRestaurant = async (
+  restaurantCity,
+  restaurantName,
+  page,
+  size,
+  sort
+) => {
+  const URL = `${END_POINTS.RESTAURANT_CONTROLLER.GET_RESTAURANT_FİLTERED}?city=${restaurantCity}&restaurantName=${restaurantName}&page=${page}&size=${size}&sortField=${sort}`;
+
+  const response = await axios.get(URL);
+  return response.data;
+};
+
+export const useGetFilteredRest = (
+  restaurantCity,
+  restaurantName,
+  page,
+  size,
+  sort
+) => {
+  const { data, isLoading, refetch } = useQuery(
+    ["filteredRestaurant", restaurantCity, restaurantName, page, size, sort],
+    () => getFilterRestaurant(restaurantCity, restaurantName, page, size, sort)
+  );
+
+  return { data, isLoading, refetch };
+};
+
+//*============ GET FAVORİTE RESTAURANT==============
+
+const getTokenFromSessionStorage = () => {
+  return JSON.parse(sessionStorage.getItem("token")).accessToken;
+};
+
+const getFavorites = async (userId) => {
+  const URL = END_POINTS.FAVORİTE_CONTROLLER.GET_FAVORİTE_RESTAURANTS.replace(
+    ":userId",
+    userId
+  );
+
+  const token = getTokenFromSessionStorage();
+
+  const headers = {
+    Authorization: `Bearer ${token}`,
+  };
+
+  const response = await axios.get(URL, { headers });
+  return response.data;
+};
+
+export const useGetFavorites = (userId) => {
+  const { data, isLoading } = useQuery(["getFavorites", userId], () =>
+    getFavorites(userId)
+  );
+
+  return { isLoading, data };
+};
+
+//*=============== CREATE FAV RESTAURANT=============
+
+const getToken = () => {
+  return JSON.parse(sessionStorage.getItem("token")).accessToken;
+};
+
+const postFavorite = async ({ userId, restaurantId }) => {
+  const URL = `http://192.168.1.25:8082/api/favorite/toggle`;
+
+  const myToken = getToken();
+  const headers = {
+    Authorization: `Bearer ${myToken}`,
+  };
+
+  const response = await axios.post(URL, { userId, restaurantId }, { headers });
+
+  return response.data;
+};
+
+export const useCreateFavorite = () => {
+  return useMutation(postFavorite);
+};
