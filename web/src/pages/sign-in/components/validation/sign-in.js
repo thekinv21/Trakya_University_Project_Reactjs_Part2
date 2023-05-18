eebd7307de760991c289b1a9ffed45cdaff3c92c
thekinv21 <vadimkiniabaev@gmail.com>
2023-05-18 03:25:29 +0300

    "deleted"

--- web/src/pages/sign-in/components/validation/sign-in.js
+++
@@ -1,8 +0,0 @@
-import * as yup from "yup";
-
-const signInSchema = yup.object().shape({
-  username: yup.string().required("Username boş birakilamaz"),
-  password: yup.string().required("Parola boş birakilamaz"),
-});
-
-export default signInSchema;
