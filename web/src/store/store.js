c378933ffedb9e591b9381159e904f7267f12551
thekinv21 <vadimkiniabaev@gmail.com>
2023-05-19 23:16:14 +0300

    update store

--- web/src/store/store.js
+++ web/src/store/store.js
@@ -1,13 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import AuthSlicer from "../store/auth/auth.slice";
 import LinksSlicer from "./route-link/LinksSlicer";
-import reservedRestaurantSlice from "./reserved/reserved.restaurant.slice";
 
 export const store = configureStore({
   reducer: {
     Link: LinksSlicer,
     Auth: AuthSlicer,
-    Reserved: reservedRestaurantSlice,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
