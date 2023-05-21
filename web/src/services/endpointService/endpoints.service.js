04921f33eabce570eb42aa441c14662ac6404a95
thekinv21 <vadimkiniabaev@gmail.com>
2023-05-22 02:56:19 +0300

    create review and menu endpoint

--- web/src/services/endpointService/endpoints.service.js
+++ web/src/services/endpointService/endpoints.service.js
@@ -17,7 +17,8 @@ export const END_POINTS = {
     GET_RESTAURANT_EMPTY_HOURS: `${BASE_URL}/api/restaurant/:id/book/slots?localDate=:date`,
     GET_RESTAURANT_WITH_PARAMS: `${BASE_URL}/api/restaurant?page=:page&size=:size`,
     GET_RESTAURANT_FILTERED: `${BASE_URL}/api/restaurant`,
-    GET_RESTAURANT_REVİEWS : `${BASE_URL}/api/review/restaurant/:restId`
+    GET_RESTAURANT_REVİEWS : `${BASE_URL}/api/review/restaurant/:restId`,
+    POST_RESTAURANT_REVİEW : `${BASE_URL}/api/review`
   },
   FAVORITE_CONTROLLER: {
     POST_FAVORITE_RESTAURANT: `${BASE_URL}/api/favorite/toggle`,
