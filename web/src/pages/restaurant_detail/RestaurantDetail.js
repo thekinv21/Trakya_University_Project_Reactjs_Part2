d105bf5a9c511c260dbe7636c77419ce632d8cbf
thekinv21 <vadimkiniabaev@gmail.com>
2023-05-27 18:32:23 +0300

    refactoring component

--- web/src/pages/restaurant_detail/RestaurantDetail.js
+++ web/src/pages/restaurant_detail/RestaurantDetail.js
@@ -8,38 +8,25 @@ import { useGetRestaurantById } from "../../services/restaurantService/restauran
 import ReservedForm from "./components/ReserveForm/ReservedForm";
 
 const RestaurantDetail = () => {
-  //?====================Seçtiğimiz Restoranin id'si==================
   const { id } = useParams();
-
-  //?====================Restoranin id'sine istek atıldi===============
-
   const { data: restaurantInfo, isLoading } = useGetRestaurantById(id);
 
-  //?====================Bilgileri alındıysa return et=================
   if (isLoading) return restaurantInfo;
 
   return (
     <Container>
-      {/*==================???RESTAURANT MAİN CONTAİNER========???==*/}
       <Box className="container p-5" sx={styles.container}>
-        {/*========================RESTAURANT CONTENT===============*/}
         <Box sx={styles.content}>
           <Box sx={styles.imageblock}>
-            {/*====================Restaurant FOTO==================*/}
             <Image
               draggable={false}
               src={restaurantInfo.restaurantImage}
               sx={styles.image}
             />
           </Box>
-          {/*=================Restaurant Tab Header=================*/}
           <RestTabMenu restaurantInfo={restaurantInfo} />
         </Box>
-
-        {/*=================Restaurant RESERVE FORM=================*/}
         <ReservedForm restaurantInfo={restaurantInfo} />
-
-        {/*=====================ALERT CONTAİNER=====================*/}
         <ToastContainer />
       </Box>
     </Container>
