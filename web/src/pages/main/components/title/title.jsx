6f0f85b23478959d157b73bf885664514f175324
thekinv21 <vadimkiniabaev@gmail.com>
2023-06-06 17:12:53 +0300

    update main titles

--- web/src/pages/main/components/title/title.jsx
+++ web/src/pages/main/components/title/title.jsx
@@ -1,28 +1,29 @@
-import React from "react";
-import { Heading, Divider, Stack } from "@chakra-ui/react";
+import { Heading, Divider, Stack } from '@chakra-ui/react'
+import React from 'react'
 
 const Title = ({ title }) => {
-  return (
-    <React.Fragment>
-      <Stack  alignItems='center'>
-        <Heading
-          color="rgb(0,0,0,0.6)"
-          fontWeight="600"
-          fontFamily="unset"
-          pb={5}
-        >
-          {title}
-        </Heading>
+	return (
+		<React.Fragment>
+			<Stack alignItems='center'>
+				<Heading
+					color='rgb(0,0,0,0.6)'
+					fontWeight='600'
+					fontFamily='unset'
+					pb={5}
+					textAlign='center'
+				>
+					{title}
+				</Heading>
 
-        <Divider
-          orientation="horizontal"
-          colorScheme="red"
-          w={["300px", "400px", "500px", "700px"]}
-          border="1px solid orange"
-        />
-      </Stack>
-    </React.Fragment>
-  );
-};
+				<Divider
+					orientation='horizontal'
+					colorScheme='red'
+					w={['300px', '400px', '500px', '700px']}
+					border='1px solid orange'
+				/>
+			</Stack>
+		</React.Fragment>
+	)
+}
 
-export default Title;
+export default Title
