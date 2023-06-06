ddad23c59e5669e3290c336066649342b6f3bff8
thekinv21 <vadimkiniabaev@gmail.com>
2023-06-06 18:27:11 +0300

    refactoring code

--- web/src/pages/main/Main.js
+++ web/src/pages/main/Main.js
@@ -1,30 +1,30 @@
-import React from "react";
-import MainContainer from "../../components/container/MainContainer";
-import Banner from "./components/banner/Banner";
-import Aminities from "./components/amenities/Aminities";
-import RestList from "./components/restaurant_list/RestList";
-import Quetions from "./components/questions/Quetions";
-import Categories from "./components/category/Categories";
+import MainContainer from '../../components/container/MainContainer'
+import Aminities from './components/amenities/Aminities'
+import Banner from './components/banner/Banner'
+import Categories from './components/category/Categories'
+import Quetions from './components/questions/Quetions'
+import RestList from './components/restaurant_list/RestList'
+import React from 'react'
 
 const Main = () => {
-  return (
-    <React.Fragment>
-      {/*========================CONTAİNER===================== */}
-      <MainContainer>
-        {/*========================BANNER===================== */}
-        <Banner />
-        {/*========================RESTAURANTLİST=================*/}
-        <RestList />
+	return (
+		<React.Fragment>
+			{/*========================CONTAİNER===================== */}
+			<MainContainer>
+				{/*========================BANNER===================== */}
+				<Banner />
+				{/*========================RESTAURANTLİST=================*/}
+				<RestList />
 
-        {/*========================AMINITIES===================== */}
-        <Aminities />
-        {/*=====================Menus Category===================== */}
-        <Categories />
-        {/*=====================User Quetions======================= */}
-        <Quetions />
-      </MainContainer>
-    </React.Fragment>
-  );
-};
+				{/*========================AMINITIES===================== */}
+				<Aminities />
+				{/*=====================Menus Category===================== */}
+				<Categories />
+				{/*=====================User Quetions======================= */}
+				<Quetions />
+			</MainContainer>
+		</React.Fragment>
+	)
+}
 
-export default Main;
+export default Main
