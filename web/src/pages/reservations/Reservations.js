236787928a6f5e2a6a296fdf19dbce4f974b35c8
thekinv21 <vadimkiniabaev@gmail.com>
2023-06-06 17:12:35 +0300

    updated

--- web/src/pages/reservations/Reservations.js
+++ web/src/pages/reservations/Reservations.js
@@ -1,35 +1,35 @@
-import Container from "../../components/container/Container";
-import UserCard from "../../components/cards/user/UserCard";
-import { Box } from "@chakra-ui/react";
-import { useResevation } from "./useReservation";
-import ReservationFilter from "./components/ReservationFilter/ReservationFilter";
-import { ToastContainer } from "react-toastify";
+import Container from '../../components/container/Container'
+import UserCard from '../profile/components/user-card/UserCard'
+import ReservationFilter from './components/ReservationFilter/ReservationFilter'
+import { useResevation } from './useReservation'
+import { Box } from '@chakra-ui/react'
+import { ToastContainer } from 'react-toastify'
 
 const Reservations = () => {
-  const { data, isLoading } = useResevation();
-  if (isLoading) return data;
+	const { data, isLoading } = useResevation()
+	if (isLoading) return data
 
-  return (
-    <Container>
-      <Box className="container-sm p-5" display="flex" gap={5}>
-        {/*==================RESERVATİON PAGE USERCARD============*/}
-        <UserCard />
+	return (
+		<Container>
+			<Box className='container-sm p-5' display='flex' gap={5}>
+				{/*==================RESERVATİON PAGE USERCARD============*/}
+				<UserCard />
 
-        {/*==================RESERVATİONS CONTAİNER============== */}
-        <Box
-          className="container-sm"
-          display="flex"
-          flexDirection="column"
-          gap={5}
-        >
-          {/*==================RESERVATİON FİLTER CARD=============*/}
+				{/*==================RESERVATİONS CONTAİNER============== */}
+				<Box
+					className='container-sm'
+					display='flex'
+					flexDirection='column'
+					gap={5}
+				>
+					{/*==================RESERVATİON FİLTER CARD=============*/}
 
-          <ReservationFilter reserveData={data} />
-          <ToastContainer />
-        </Box>
-      </Box>
-    </Container>
-  );
-};
+					<ReservationFilter reserveData={data} />
+					<ToastContainer />
+				</Box>
+			</Box>
+		</Container>
+	)
+}
 
-export default Reservations;
+export default Reservations
