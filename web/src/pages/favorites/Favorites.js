c0bd0dde85d898339938d04abb8dc9acf57865fb
thekinv21 <vadimkiniabaev@gmail.com>
2023-06-06 17:13:24 +0300

    updated

--- web/src/pages/favorites/Favorites.js
+++ web/src/pages/favorites/Favorites.js
@@ -1,54 +1,54 @@
-import Container from "../../components/container/Container";
-import { Box } from "@chakra-ui/react";
-import RestaurantCard from "../main/components/restaurant_list/RestaurantCard/RestaurantCard";
-import { useFavorites } from "./useFavorites";
-import FavoriteNotFound from "./components/FavoriteNotFound";
-import UserCard from "./../../components/cards/user/UserCard";
+import Container from '../../components/container/Container'
+import RestaurantCard from '../main/components/restaurant_list/RestaurantCard/RestaurantCard'
+import UserCard from '../profile/components/user-card/UserCard'
+import FavoriteNotFound from './components/FavoriteNotFound'
+import { useFavorites } from './useFavorites'
+import { Box } from '@chakra-ui/react'
 
 const Favorites = () => {
-  const { onClick, isLoading, filteredData, data } = useFavorites();
+	const { onClick, isLoading, filteredData, data } = useFavorites()
 
-  if (isLoading) return data;
+	if (isLoading) return data
 
-  return (
-    <Container>
-      {/*=================FAVORİTES CONTAİNER===============*/}
-      <Box
-        padding={10}
-        className="container"
-        display="flex"
-        alignItems="flex-start"
-        justifyContent="center"
-        gap={5}
-      >
-        {/*===================FAVORİ USER CARD================= */}
-        <UserCard />
+	return (
+		<Container>
+			{/*=================FAVORİTES CONTAİNER===============*/}
+			<Box
+				padding={10}
+				className='container'
+				display='flex'
+				alignItems='flex-start'
+				justifyContent='center'
+				gap={5}
+			>
+				{/*===================FAVORİ USER CARD================= */}
+				<UserCard />
 
-        {/*================FAVORİ RESTAURANT VARSA============ */}
+				{/*================FAVORİ RESTAURANT VARSA============ */}
 
-        <Box
-          display="flex"
-          justifyContent={["center", "center", "center", "flex-start"]}
-          alignItems="center"
-          flexWrap="wrap"
-          gap={5}
-        >
-          {filteredData?.length ? (
-            filteredData?.map((fav, index) => (
-              <RestaurantCard
-                key={index}
-                onClick={onClick}
-                restaurant={{ ...fav, isFavorite: true }}
-                favData={data.content}
-              />
-            ))
-          ) : (
-            <FavoriteNotFound />
-          )}
-        </Box>
-      </Box>
-    </Container>
-  );
-};
+				<Box
+					display='flex'
+					justifyContent={['center', 'center', 'center', 'flex-start']}
+					alignItems='center'
+					flexWrap='wrap'
+					gap={5}
+				>
+					{filteredData?.length ? (
+						filteredData?.map((fav, index) => (
+							<RestaurantCard
+								key={index}
+								onClick={onClick}
+								restaurant={{ ...fav, isFavorite: true }}
+								favData={data.content}
+							/>
+						))
+					) : (
+						<FavoriteNotFound />
+					)}
+				</Box>
+			</Box>
+		</Container>
+	)
+}
 
-export default Favorites;
+export default Favorites
