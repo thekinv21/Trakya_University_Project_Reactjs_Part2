3f4fb7585f3c0c6f8c9ced5bc100daaa5dde2f6a
thekinv21 <vadimkiniabaev@gmail.com>
2023-06-06 15:06:50 +0300

    updated

--- web/src/components/shared/Input/Inputs.js
+++ web/src/components/shared/Input/Inputs.js
@@ -1,55 +1,55 @@
-import { Input } from "@chakra-ui/react";
+import { Input } from '@chakra-ui/react'
 
 const Inputs = ({
-  id,
-  type,
-  name,
-  width,
-  height,
-  background,
-  color,
-  fontsize,
-  fontweight,
-  value,
-  onchange,
-  placeholder,
-  border,
-  outline,
-  onblur,
-  variant,
-  focusBorderColor,
-  borderLeftRadius,
-  borderRightRadius,
-  size,
-  maxLength,
+	id,
+	type,
+	name,
+	width,
+	height,
+	background,
+	color,
+	fontsize,
+	fontweight,
+	value,
+	onchange,
+	placeholder,
+	border,
+	outline,
+	onblur,
+	variant,
+	focusBorderColor,
+	borderLeftRadius,
+	borderRightRadius,
+	size,
+	maxLength,
 }) => {
-  return (
-    <Input
-      size={size}
-      type={type}
-      name={name}
-      w={width}
-      h={height}
-      bg={background}
-      color={color}
-      placeholder={placeholder}
-      fontSize={fontsize}
-      fontWeight={fontweight}
-      value={value}
-      onChange={onchange}
-      border={border}
-      outline={outline}
-      autoComplete="off"
-      _placeholder={{ fontSize: "12px" }}
-      onBlur={onblur}
-      variant={variant}
-      id={id}
-      focusBorderColor={focusBorderColor}
-      borderLeftRadius={borderLeftRadius}
-      borderRightRadius={borderRightRadius}
-      maxLength={maxLength}
-    />
-  );
-};
+	return (
+		<Input
+			size={size}
+			type={type}
+			name={name}
+			w={width}
+			h={height}
+			bg={background}
+			color={color}
+			placeholder={placeholder}
+			fontSize={fontsize}
+			fontWeight={fontweight}
+			value={value}
+			onChange={onchange}
+			border={border}
+			outline={outline}
+			autoComplete='off'
+			_placeholder={{ fontSize: '12px' }}
+			onBlur={onblur}
+			variant={variant}
+			id={id}
+			focusBorderColor={focusBorderColor}
+			borderLeftRadius={borderLeftRadius}
+			borderRightRadius={borderRightRadius}
+			maxLength={maxLength}
+		/>
+	)
+}
 
-export default Inputs;
+export default Inputs
