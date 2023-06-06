1d4175049b4f5e7fa351a419a6ad0ac7045acf7c
thekinv21 <vadimkiniabaev@gmail.com>
2023-06-06 21:03:18 +0300

    updated

--- web/src/pages/restaurant_detail/components/RestTabMenu/components/RestReviews/components/ReviewForm/ReviewForm.js
+++ web/src/pages/restaurant_detail/components/RestTabMenu/components/RestReviews/components/ReviewForm/ReviewForm.js
@@ -1,46 +1,46 @@
-import React from "react";
-import { Form, Input, Rate, Button } from "antd";
-import styles from "./ReviewForm.module.css";
-import { useReviewForm } from "./useReviewForm";
+import styles from './ReviewForm.module.css'
+import { useReviewForm } from './useReviewForm'
+import { Form, Input, Rate, Button } from 'antd'
+import React from 'react'
 
 const ReviewForm = ({ restaurantId, refetch }) => {
-  const { onFinish } = useReviewForm({ restaurantId, refetch });
+	const { onFinish, desc } = useReviewForm({ restaurantId, refetch })
 
-  return (
-    <Form onFinish={onFinish} className={styles.form}>
-      <div className={styles.header}>
-        <Form.Item name="name">
-          <h1 className={styles.header_title}>Restorana Yorum Birakiniz!</h1>
-        </Form.Item>
-        <Form.Item name="rating" required>
-          <Rate />
-        </Form.Item>
-      </div>
+	return (
+		<Form onFinish={onFinish} className={styles.form}>
+			<div className={styles.header}>
+				<Form.Item name='name'>
+					<h1 className={styles.header_title}>Restorana Yorum Birakiniz!</h1>
+				</Form.Item>
+				<Form.Item name='rating' required initialValue={1}>
+					<Rate tooltips={desc} allowClear={false} />
+				</Form.Item>
+			</div>
 
-      <Form.Item name="name">
-        <Input placeholder="Adinizi giriniz" autoComplete="off" required />
-      </Form.Item>
+			<Form.Item name='name'>
+				<Input placeholder='Adinizi giriniz' autoComplete='off' required />
+			</Form.Item>
 
-      <Form.Item name="review">
-        <Input.TextArea
-          autoSize={{ minRows: 2, maxRows: 6 }}
-          placeholder="Herhangi bir yorum!"
-          rows={4}
-          required
-        />
-      </Form.Item>
+			<Form.Item name='review'>
+				<Input.TextArea
+					autoSize={{ minRows: 2, maxRows: 6 }}
+					placeholder='Herhangi bir yorum!'
+					rows={4}
+					required
+				/>
+			</Form.Item>
 
-      <Form.Item>
-        <Button
-          type="primary"
-          htmlType="submit"
-          style={{ width: "100%", fontWeight: "100" }}
-        >
-          Yorum Yaz!
-        </Button>
-      </Form.Item>
-    </Form>
-  );
-};
+			<Form.Item>
+				<Button
+					type='primary'
+					htmlType='submit'
+					style={{ width: '100%', fontWeight: '100' }}
+				>
+					Yorum Yaz!
+				</Button>
+			</Form.Item>
+		</Form>
+	)
+}
 
-export default ReviewForm;
+export default ReviewForm
--- web/src/pages/restaurant_detail/components/RestTabMenu/components/RestReviews/components/ReviewForm/useReviewForm.js
+++ web/src/pages/restaurant_detail/components/RestTabMenu/components/RestReviews/components/ReviewForm/useReviewForm.js
@@ -1,36 +1,38 @@
-import { toast } from "react-toastify";
-import { usePostReview } from "../../../../../../../../services/restaurantService/restaurant.service";
+import { usePostReview } from '../../../../../../../../services/restaurantService/restaurant.service'
+import { toast } from 'react-toastify'
 
 export const useReviewForm = ({ restaurantId, refetch }) => {
-  const userDetail = JSON.parse(sessionStorage.getItem("token"));
+	const userDetail = JSON.parse(sessionStorage.getItem('token'))
 
-  const { mutate: CreateReview } = usePostReview();
+	const { mutate: CreateReview } = usePostReview()
 
-  const onFinish = async (values) => {
-    const requestData = {
-      userId: userDetail.user.id,
-      restaurantId: restaurantId,
-      star: values.rating,
-      content: values.review,
-    };
+	const onFinish = async values => {
+		const requestData = {
+			userId: userDetail.user.id,
+			restaurantId: restaurantId,
+			star: values.rating,
+			content: values.review,
+		}
 
-    new Promise(() => {
-      CreateReview(requestData, {
-        onSuccess: () => {
-          toast.success("Restorana Yorum Yaptiniz!");
-          setTimeout(() => {
-            refetch();
-          }, 3000);
-        },
+		new Promise(() => {
+			CreateReview(requestData, {
+				onSuccess: () => {
+					toast.success('Restorana Yorum Yaptiniz!')
+					setTimeout(() => {
+						refetch()
+					}, 3000)
+				},
 
-        onError: () => {
-          toast.error("Yorum Yaparken Hata oluştu!");
-        },
-      });
-    });
-  };
+				onError: error => {
+					toast.error(error.response.data.error)
+				},
+			})
+		})
+	}
 
-  return {
-    onFinish,
-  };
-};
+	const desc = ['Berbat', 'Kötü', 'Normal', 'İyi', 'Muhteşem']
+	return {
+		onFinish,
+		desc,
+	}
+}
